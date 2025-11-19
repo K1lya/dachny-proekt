f3b15b6d2ca7a182761de1724d59d30906a2bdd8
Danila Kilishov <danila.kilishov@x5.ru>
2025-11-19 09:56:12 +0300

    feat: add aliases, add styles to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react';
 // @ts-ignore
 import checker from 'vite-plugin-checker';
 import svgr from 'vite-plugin-svgr';
+import * as path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     svgr(),
