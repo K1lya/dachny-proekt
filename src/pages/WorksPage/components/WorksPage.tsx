806f0027b79fc3b6ef2b7f16b2f0266445eec678
Danila Kilishov <danila.kilishov@x5.ru>
2025-12-10 17:15:14 +0300

    feat: WorksPage

--- src/pages/WorksPage/components/WorksPage.tsx
+++ src/pages/WorksPage/components/WorksPage.tsx
@@ -1,14 +1,52 @@
 import styles from './WorksPage.module.css';
-import { WorkCard } from '@/shared/ui/WorkCard/WorkCard.tsx';
+import { Text } from '@/shared/ui/Text/Text.tsx';
+import { Button } from '@/shared/ui/Button/Button.tsx';
+import img from '@/shared/assets/icons/worksPage/img.png';
 import { works } from '@/shared/constants/worksPage/works.ts';
+import { WorkCard } from '@/shared/ui/WorkCard/WorkCard.tsx';
 
 export const WorksPage = () => {
   // consts
   return (
     <section className={styles.root}>
-      {works.map((work) => (
-        <WorkCard key={work.title} {...work} />
-      ))}
+      <div className={styles.info}>
+        <div>
+          <Text weight={400} size={'56px'}>
+            Услуги
+          </Text>
+          <div className={styles.border} />
+        </div>
+        <Text weight={500} size={'31px'} className={styles.infoText}>
+          Все виды работ
+          <br />с деревянными <br />
+          конструкциями
+        </Text>
+        <Text weight={500} size={'19px'} className={styles.infoText}>
+          Выполняем полный спектр плотницких и столярных работ: монтаж, замена,
+          обработка, утепление, отделка, антисептическая и противопожарная защита.
+          Работаем с любыми деревянными элементами — стенами, перекрытиями, кровлей,
+          полами и декоративными деталями.
+        </Text>
+        <div className={styles.buttonContainer}>
+          <div className={styles.image}>
+            <img src={img} className={styles.img} />
+          </div>
+          <Button
+            width={'322px'}
+            weight={600}
+            height={'61px'}
+            fontSize={'19px'}
+            className={styles.button}
+          >
+            ОСТАВИТЬ ЗАЯВКУ
+          </Button>
+        </div>
+      </div>
+      <div className={styles.works}>
+        {works.map((work) => (
+          <WorkCard key={work.title} {...work} />
+        ))}
+      </div>
     </section>
   );
 };
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -9,6 +9,7 @@ interface ButtonProps {
   fontSize?: string;
   weight?: number;
   width?: string;
+  height?: string;
 }
 
 export const Button: FC<PropsWithChildren<ButtonProps>> = (props) => {
@@ -18,6 +19,7 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = (props) => {
     children,
     onClick,
     width = '60px',
+    height,
     fontSize = '16px',
     weight = 600,
   } = props;
@@ -25,7 +27,7 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = (props) => {
     <button
       onClick={onClick}
       className={clsx(styles.root, className)}
-      style={{ width }}
+      style={{ width, height }}
     >
       <Text size={fontSize} weight={weight} color='#FFFFFF'>
         {children}
