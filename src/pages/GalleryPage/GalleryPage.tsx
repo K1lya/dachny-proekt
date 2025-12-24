79f9a80957ff3169f492fb1c6d0bff7dc4356228
Danila Kilishov <danila.kilishov@x5.ru>
2025-12-25 01:34:57 +0300

    feat: GalleryPage tablet view

--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import styles from './GalleryPage.module.css';
 import { Gallery } from '@/features/Gallery';
 import { Text } from '@/shared/ui/Text/Text.tsx';
+import { EView, useView } from '@/shared/hooks/useView.ts';
 
 interface GalleryPageProps {
   className?: string;
@@ -11,12 +12,24 @@ interface GalleryPageProps {
 export const GalleryPage: FC<PropsWithChildren<GalleryPageProps>> = (props) => {
   // consts
   const { className } = props;
+  const view = useView();
   return (
-    <section className={clsx(styles.root, className)}>
+    <section
+      className={clsx(
+        styles.root,
+        { [styles.rootTablet]: view === EView.TABLET },
+        className,
+      )}
+    >
       <div className={styles.titleContainer}>
-        <Text weight={400} size='56px' className={styles.title}>
+        <Text
+          weight={400}
+          size={view === EView.TABLET ? '36px' : '56px'}
+          className={styles.title}
+        >
           Вдохновляйтесь уютом,
-          <br /> который мы создаём
+          <br />
+          который мы создаём
         </Text>
       </div>
       <Gallery />
