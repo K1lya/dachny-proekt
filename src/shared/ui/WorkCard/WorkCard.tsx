b6ec6beb741e3a434f80a025808d3738b71bb294
Danila Kilishov <danila.kilishov@x5.ru>
2026-01-25 12:02:37 +0300

    feat: fix WorkCard

--- src/shared/ui/WorkCard/WorkCard.tsx
+++ src/shared/ui/WorkCard/WorkCard.tsx
@@ -14,19 +14,16 @@ interface WorkCardProps {
 const Chevron = ({ isOpen }: { isOpen: boolean }) => (
   <svg
     className={clsx(styles.chevron, isOpen && styles.chevronOpen)}
-    width='20'
-    height='20'
-    viewBox='0 0 20 20'
+    width='19'
+    height='12'
+    viewBox='0 0 19 12'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
-    aria-hidden
   >
     <path
-      d='M5 8L10 13L15 8'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
+      d='M1.06067 1.06055L9.06067 9.06055L17.0607 1.06055'
+      stroke='#E85A4F'
+      stroke-width='3'
     />
   </svg>
 );
