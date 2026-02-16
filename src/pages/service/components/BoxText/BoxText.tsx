dfb2eb7bcc299da2623e94ff584b4c4ffd4c80e7
Danila Kilishov <danila.kilishov@x5.ru>
2026-02-16 23:59:22 +0300

    feat: add BoxText

---
+++ src/pages/service/components/BoxText/BoxText.tsx
@@ -0,0 +1,53 @@
+import type { FC, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import styles from './BoxText.module.css';
+import { EView, useView } from '@/shared/hooks/useView.ts';
+import { Text } from '@/shared/ui/Text/Text.tsx';
+
+interface BoxTextProps {
+  text: string;
+  title?: string;
+}
+
+export const BoxText: FC<PropsWithChildren<BoxTextProps>> = (props) => {
+  // consts
+  const { text, title } = props;
+  const view = useView();
+  const textSize =
+    view === EView.TABLET ? '16px' : view === EView.MOBILE ? '12px' : '18px';
+  const titleSize =
+    view === EView.TABLET ? '24px' : view === EView.MOBILE ? '16px' : '36px';
+  return (
+    <div
+      className={clsx(
+        styles.root,
+        {
+          [styles.rootTablet]: view === EView.TABLET,
+          [styles.rootMobile]: view === EView.MOBILE,
+        },
+        'serviceContainer',
+      )}
+    >
+      <Text
+        size={titleSize}
+        weight={500}
+        className={clsx(styles.title, {
+          [styles.titleTablet]: view === EView.TABLET,
+          [styles.titleMobile]: view === EView.MOBILE,
+        })}
+      >
+        {title}
+      </Text>
+      <Text
+        size={textSize}
+        weight={400}
+        className={clsx(styles.text, {
+          [styles.textTablet]: view === EView.TABLET,
+          [styles.textMobile]: view === EView.MOBILE,
+        })}
+      >
+        {text}
+      </Text>
+    </div>
+  );
+};
--- src/pages/service/components/Service/Service.tsx
+++ src/pages/service/components/Service/Service.tsx
@@ -10,6 +10,7 @@ import { Title } from '@/pages/service/components/Title/Title.tsx';
 import { DotsText } from '@/pages/service/components/DotsText/DotsText.tsx';
 import { AlertText } from '@/pages/service/components/AlertText/AlertText.tsx';
 import { NumberText } from '@/pages/service/components/NumberText/NumberText.tsx';
+import { BoxText } from '@/pages/service/components/BoxText/BoxText.tsx';
 
 const Service = () => {
   // consts
@@ -77,6 +78,13 @@ const Service = () => {
             ' и износостойкое строение, необходимо более внимательно подходить' +
               '  как это вряд ли говорит о высоком профессионализме сотрудников.',
           ]}
+        />
+        <BoxText
+          text={
+            // eslint-disable-next-line max-len
+            'Для того чтобы создать действительно качественное и износостойкое строение, необходимо более внимательно подходить к выбору строительных материалов. При заказе строительства деревянных домов  в одной из компаний, вы можете воспользоваться услугой, при которой представитель строительной организации сопровождает клиента в магазин и помогает с выбором всей необходимой продукции. Специалисты имеют большой опыт в данном вопросе, поэтому без труда смогут определить качественный товар.'
+          }
+          title={'Выделенный текст + Заголовок '}
         />
       </div>
     </div>
